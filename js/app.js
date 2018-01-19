f2735132418c8b3eca5f3533a9c8a8bcac2334b3
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-19 11:33:58 -0500

    not sure it previous fix is working added TESTING to the slack message

--- js/app.js
+++ js/app.js
@@ -70,7 +70,7 @@ $(document).ready(function() {
 
 
   var url = "https://hooks.slack.com/services/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";
-  var text = "☕️ Time for " + makeCoffee(pickRandom) + " to make café. 👏🏽TATA TATATATA TATA👏🏽";
+  var text = "TESTING-->" + " ☕️ Time for " + makeCoffee(pickRandom) + " to make café. 👏🏽TATA TATATATA TATA👏🏽";
   var ajaxReq = function () {
       $.ajax({
           data: 'payload=' + JSON.stringify({
